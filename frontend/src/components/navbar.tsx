76d7bb81def1de3859b6b94904e7ea5fa147e325
Yousof Algburi <yousofalgburi@Yousofs-MacBook-Pro.local>
2023-02-16 21:49:51 -0500

    getting started on pagination work for main page

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -84,7 +84,7 @@ export default function Navbar() {
 												<span className='sr-only'>Open user menu</span>
 												<img
 													className='h-8 w-8 rounded-full'
-													src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+													src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
 													alt=''
 												/>
 											</Menu.Button>
